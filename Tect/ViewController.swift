dcff5de8ecc9c9dc5fa07ae9e2ddaca0456ad36f
noah <noahwoodward2244@gmail.com>
2018-10-20 19:50:18 -0700

    Initial Commit

---
+++ Tect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tect
+//
+//  Created by Noah Woodward on 10/20/18.
+//  Copyright © 2018 Noah Woodward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
